9f0386e1a876a0a65156a34f9d48ad3ef3e57b10
Alexander-Sobolev <sobolev.ios.developer@gmail.com>
2021-09-21 20:26:30 +0300

    Initial Commit

---
+++ AnimatedChartSwiftUI/AnimatedChartSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedChartSwiftUIApp.swift
+//  AnimatedChartSwiftUI
+//
+//  Created by Alexander Sobolev on 21.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedChartSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
